cf4484f2beccfd8988bc97905809223f15a8eb62
swoerve <swoerve@gmail.com>
2025-10-25 13:36:43 +0200

    quick changes to base boiler

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ app.get('/api', (_request, response) => {
     response.send({hello: 'World'})
 })
 
-app.use(express.static(path.join(path.resolve(), 'dist/browser')))
+app.use(express.static(path.join(path.resolve(), 'dist/frontend/browser')))
 
 app.listen(3000, ()=> {
     console.log('Backend ready at http://localhost:3000')
