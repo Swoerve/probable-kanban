66037bf5b25329a46863eebc7ae48b1b0cf06c3c
swoerve <swoerve@gmail.com>
2025-10-27 11:47:05 +0100

    moved things around

--- backend/index.js
+++ backend/index.js
@@ -6,11 +6,7 @@ const routes = require('./router')
 
 
 app.use(cors())
-app.use('/api', express.json())
 
-app.get('/api', (_request, response) => {
-    response.send({hello: 'World'})
-})
 app.use('/api', routes)
 app.use(express.static(path.join(path.resolve(), 'dist/frontend/browser')))
 
--- backend/router.js
+++ backend/router.js
@@ -2,6 +2,12 @@ const express = require('express')
 const router = express.Router()
 const controller = require('./controller')
 
+router.use(express.json())
+
+// get a hello world response, mostly for testing
+router.get('/', (req, res) => {
+    res.send({hello: 'World'})
+})
 
 // get a specific board
 router.get('/board/:id', controller.getBoard)
